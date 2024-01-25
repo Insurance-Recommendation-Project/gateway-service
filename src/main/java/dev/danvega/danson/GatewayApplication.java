0d79aed03f74c6e1034c7959b467a49ae9c2e020
ELMACHKOURIImane <imane.elmachkouri2@gmail.com>
2024-01-25 21:29:53 +0100

    mdify the configuration

--- src/main/java/dev/danvega/danson/GatewayApplication.java
+++ src/main/java/dev/danvega/danson/GatewayApplication.java
@@ -13,7 +13,7 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Bean
+	//@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/auth/**")
